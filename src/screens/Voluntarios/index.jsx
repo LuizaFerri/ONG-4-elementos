2347e62ac4461c80b383c301fe24315aea471ed0
Madtheusss <89808175+Madtheusss@users.noreply.github.com>
2021-12-15 13:07:44 -0300

    Create index.jsx

---
+++ src/screens/Voluntarios/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+export default function Voluntarios(){
+    return(
+        <>
+            <Header/>
+                <div className="container">
+                    <h1>Ola mundo</h1>
+                </div>
+            <Footer/>
+        </>
+    )
+}
