9884d65fa7a1cd43646b73ef4a91b0ebc446c808
Madtheusss <89808175+Madtheusss@users.noreply.github.com>
2021-12-15 13:40:57 -0300

    Update index.jsx

--- src/screens/Voluntarios/index.jsx
+++ src/screens/Voluntarios/index.jsx
@@ -1,16 +1,55 @@
 import React from 'react';
 import './styles.css';
+import comunicacaoIcon from '../../assets/voluntario/iconeComunicacao.png';
+import estrelaIcon from '../../assets/voluntario/iconeEstrela.png';
+import trofeuIcon from '../../assets//voluntario/iconeTrofeu.png';
 
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from '../../components/Header/index';
+import Footer from '../../components/Footer/index';
+
 
 export default function Voluntarios(){
     return(
         <>
             <Header/>
-                <div className="container">
-                    <h1>Ola mundo</h1>
+            <div className="containerVol">
+                <div className="textoVolPrincipal">
+                    <h1>Volúntario</h1>
+                    <p>O Instituto 4 Elementos está sempre aberto para novos<br/>
+                        voluntários! Apreciamos toda e qualquer ajuda. Veja<br/>
+                        alguns trabalhos em que você pode se envolver:</p>
                 </div>
+            
+                <div className="caixaVolExplicacao">
+                    <div className="textoVolComunicacao">
+                        <img className="VoluntarioIcons"src={comunicacaoIcon} />
+                        <h1>Comunicação</h1>
+                        <p>Gosta de Gestão de mídias sociais? Design gráfico?<br/>
+                            Storytelling? Engajamento virtual? Precisamos de<br/>
+                            pessoas que ajudem a trazer visibilidade para o<br/>
+                            instituto e nossos projetos!</p>
+                    </div>
+                
+                    <div className="textoVolCaptacao">
+                        <img className="VoluntarioIcons" src={estrelaIcon} />
+                        <h1>Captação</h1>
+                        <p>O terceiro setor precisa muito de recursos para<br/>
+                            funcionar, e um dos mais importantes canais é a <br/>
+                            captação de recursos através das doações. <br/>
+                            Ajude-nos a encontrar mais doadores!</p>
+                    </div>
+
+                    <div className="textoVolProjetos">
+                        <img className="VoluntarioIcons"src={trofeuIcon} />
+                        <h1>Projetos</h1>
+                        <p>O Instituto 4 Elementos tem diversos projetos na <br/>
+                            manga, pequenos e grandes, precisando de mais <br/>
+                            braços para erguê-los. Venha se envolver nas<br/>
+                            nossas ações de impacto socioambiental!</p>
+                    </div>
+                </div>
+
+            </div>
             <Footer/>
         </>
     )
