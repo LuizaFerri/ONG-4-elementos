4042d876b07166d196f02dc04a1330692ce01209
Madtheusss <89808175+Madtheusss@users.noreply.github.com>
2021-12-15 15:22:26 -0300

    Update index.jsx

--- src/screens/Voluntarios/index.jsx
+++ src/screens/Voluntarios/index.jsx
@@ -9,43 +9,43 @@ import Header from '../../components/Header/index';
 import Footer from '../../components/Footer/index';
 
 
-export default function Voluntarios(){
-    return(
+export default function Voluntarios() {
+    return (
         <>
-            <Header/>
+            <Header />
             <div className="containerVol">
                 <div className="textoVolPrincipal">
                     <h1>Voluntário</h1>
-                    <p>O Instituto 4 Elementos está sempre aberto para novos<br/>
-                        voluntários! Apreciamos toda e qualquer ajuda. Veja<br/>
+                    <p>O Instituto 4 Elementos está sempre aberto para novos<br />
+                        voluntários! Apreciamos toda e qualquer ajuda. Veja<br />
                         alguns trabalhos em que você pode se envolver:</p>
                 </div>
-            
+
                 <div className="caixaVolExplicacao">
                     <div className="textoVolComunicacao">
-                        <img className="VoluntarioIcons"src={comunicacaoIcon} />
+                        <img className="VoluntarioIcons" src={comunicacaoIcon} />
                         <h1>Comunicação</h1>
-                        <p>Gosta de Gestão de mídias sociais? Design gráfico?<br/>
-                            Storytelling? Engajamento virtual? Precisamos de<br/>
-                            pessoas que ajudem a trazer visibilidade para o<br/>
+                        <p>Gosta de Gestão de mídias sociais? Design gráfico?<br />
+                            Storytelling? Engajamento virtual? Precisamos de<br />
+                            pessoas que ajudem a trazer visibilidade para o<br />
                             instituto e nossos projetos!</p>
                     </div>
-                
+
                     <div className="textoVolCaptacao">
                         <img className="VoluntarioIcons" src={estrelaIcon} />
                         <h1>Captação</h1>
-                        <p>O terceiro setor precisa muito de recursos para<br/>
-                            funcionar, e um dos mais importantes canais é a <br/>
-                            captação de recursos através das doações. <br/>
+                        <p>O terceiro setor precisa muito de recursos para<br />
+                            funcionar, e um dos mais importantes canais é a <br />
+                            captação de recursos através das doações. <br />
                             Ajude-nos a encontrar mais doadores!</p>
                     </div>
 
                     <div className="textoVolProjetos">
-                        <img className="VoluntarioIcons"src={trofeuIcon} />
+                        <img className="VoluntarioIcons" src={trofeuIcon} />
                         <h1>Projetos</h1>
-                        <p>O Instituto 4 Elementos tem diversos projetos na <br/>
-                            manga, pequenos e grandes, precisando de mais <br/>
-                            braços para erguê-los. Venha se envolver nas<br/>
+                        <p>O Instituto 4 Elementos tem diversos projetos na <br />
+                            manga, pequenos e grandes, precisando de mais <br />
+                            braços para erguê-los. Venha se envolver nas<br />
                             nossas ações de impacto socioambiental!</p>
                     </div>
                 </div>
@@ -62,10 +62,27 @@ export default function Voluntarios(){
                     </p>
                 </div>
 
-                
+                <form className='volFormulario'>
+                    <label>
+                        <span className='tituloFormulario'>Home:</span>
+                        <input className='valorInput' type="text" />  
+
+                        <span className='tituloFormulario'>Telefone:</span>
+                        <input className='valorInput' type="number" />  
+
+                        <span className='tituloFormulario'>Endereço:</span>
+                        <input className='valorInput' type="text" />
+
+                        <span className='tituloFormulario'>Conte-nos sobre você: </span>
+                        <input className='descricaoFormulario' type="text" />
+
+                        <span className='tituloFormulario'>Como gostaria de contribuir?</span>
+                        <input className='descricaoFormulario'type="text" />
+                    </label>
+                </form>
 
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 }
