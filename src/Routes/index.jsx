8c80792b047d59d022b46a5be7d938fc61a5d470
Madtheusss <89808175+Madtheusss@users.noreply.github.com>
2021-12-18 10:57:58 -0300

    Update index.jsx

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -8,6 +8,7 @@ import ProjetosAtuais from '../screens/projetosAtuais/index';
 import Doacao from '../screens/Doacao/index';
 import Voluntarios from '../screens/Voluntarios/index';
 import Associado from "../screens/Associado";
+import Contatos from "../screens/Contatos";
 
 const Routes = () => {
     return (
@@ -20,6 +21,7 @@ const Routes = () => {
             <Route component={ Doacao } path="/doacao" />
             <Route component={ Voluntarios } path="/voluntario" />
             <Route component={ Associado } path="/associado" />
+            <Route component={ Contatos } path="/contatos" />
             </Switch>
         </BrowserRouter>
     )
