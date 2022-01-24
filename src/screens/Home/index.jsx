cd74566b71bf49da291e101bcfd6797b8169aff4
Luiza de Oliveira Ferri <luizamanfrediniferri@hotmail.com>
2022-01-24 15:37:14 -0300

    up

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import './styles.css';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer'
+import React from "react";
+import "./styles.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
-
-export default function Home () {
-    return(
-        <>
-         <Header /> 
-            <div className="container2">
-                <div className="imagemHome">
-                    <img src='https://www.datocms-assets.com/60403/1640633970-imgfrase.png' alt="imagem com texto" />
-                </div>
-            </div>
-            <Footer />
-            </>
-    );
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="container2">
+        <div className="divPhome">
+           <p className="pHome">
+          A 4 Elementos é uma organização que trabalha para ampliar as
+          oportunidades de desenvolvimento socioambiental, principalmente para
+          as novas gerações.
+        </p>
+        </div>
+       
+        <div className="imagensOn">
+          <img
+            className="imgsOng"
+            src="https://www.datocms-assets.com/60403/1642195232-261300901_4339512129509320_83688705255808722_n.jpg"
+            alt="imagem ong"
+          />
+          <img
+            className="imgsOng"
+            src="https://www.datocms-assets.com/60403/1642195221-261795712_4339512119509321_3507690507967935709_n.jpg"
+            alt="imagem ong"
+          />
+          <img
+            className="imgsOng"
+            src="https://www.datocms-assets.com/60403/1642195212-263409068_4346315035495696_4517821158607606358_n.jpg"
+            alt="imagem ong"
+          />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
