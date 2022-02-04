20bb17fa48c3c8feb21584237470ed19996434cb
Luiza de Oliveira Ferri <luizamanfrediniferri@hotmail.com>
2022-02-03 21:14:11 -0300

    novos itens na home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -9,13 +9,77 @@ export default function Home() {
       <Header />
       <div className="container2">
         <div className="divPhome">
-           <p className="pHome">
-          A 4 Elementos é uma organização que trabalha para ampliar as
-          oportunidades de desenvolvimento socioambiental, principalmente para
-          as novas gerações.
-        </p>
+          <p className="pHome">
+            A 4 Elementos é uma organização que trabalha para ampliar as
+            oportunidades de desenvolvimento socioambiental, principalmente para
+            as novas gerações.
+          </p>
         </div>
-       
+        <span className="conhecaP">Conheça alguns de nossos projetos</span>
+        <div className="cards">
+          <div className="card1">
+            <p className="titleCard">Projeto: Wash nas escolas</p>
+            <p className="cardDescricao">
+              avaliar a gestão da água, saneamento e higiene em 12 escolas de
+              Anápolis e mitigar os riscos causados pela má gestão desses
+              serviços.
+            </p>
+            <div className="cardImage">
+              <img
+                src="https://www.datocms-assets.com/60403/1643050575-download.png"
+                alt="imagem-card"
+                className="imagemCard"
+              />
+            </div>
+          </div>
+          <div className="card1">
+            <p className="titleCard">Projeto: UEG Sustentável</p>
+            <p className="cardDescricao">
+              Desenvolver a ideia de sustentabilidade e concientizar a
+              comunidade universitária a respeito do consumo consciente e
+              responsabilidade compartilhada.
+            </p>
+            <div className="cardImage">
+              <img
+                src="https://www.datocms-assets.com/60403/1643929825-download.png"
+                alt="imagem-card"
+                className="imagemCardMenina"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card1">
+            <p className="titleCard">Projeto: Wash nas escolas</p>
+            <p className="cardDescricao">
+              avaliar a gestão da água, saneamento e higiene em 12 escolas de
+              Anápolis e mitigar os riscos causados pela má gestão desses
+              serviços.
+            </p>
+            <div className="cardImage">
+              <img
+                src="https://www.datocms-assets.com/60403/1643050575-download.png"
+                alt="imagem-card"
+                className="imagemCard"
+              />
+            </div>
+          </div>
+          <div className="card1">
+            <p className="titleCard">Projeto: Wash nas escolas</p>
+            <p className="cardDescricao">
+              avaliar a gestão da água, saneamento e higiene em 12 escolas de
+              Anápolis e mitigar os riscos causados pela má gestão desses
+              serviços.
+            </p>
+            <div className="cardImage">
+              <img
+                src="https://www.datocms-assets.com/60403/1643050575-download.png"
+                alt="imagem-card"
+                className="imagemCard"
+              />
+            </div>
+          </div>
+        </div>
         <div className="imagensOn">
           <img
             className="imgsOng"
